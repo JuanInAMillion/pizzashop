e53507ae1f8007d07d38c34a96623ae22669fbf0
JuanInAMillion <juan231f@yahoo.com>
2021-04-17 04:15:12 -0400

    converted layout into an Angular Project

---
+++ PizzaShop/src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { OrdernowComponent } from './components/ordernow/ordernow.component';
+import { AccountComponent } from './components/account/account.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { //this makes the home page the default path
+    path: '',   
+    redirectTo: '/home', 
+    pathMatch: 'full' },
+  { 
+    path: 'home', 
+    component: HomeComponent
+  },
+  { 
+    path: 'about', 
+    component: AboutComponent 
+  },
+  { 
+    path: 'menu', 
+    component: MenuComponent
+  },
+  {
+    path: 'ordernow',
+    component: OrdernowComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  },
+  {
+    path: '**', 
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
